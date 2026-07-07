45a687d835fe79a106e65965d7f12d7dafd1d645
Pooja-Rajput05 <pooja.kv2005@gmail.com>
2026-07-07 22:49:05 +0530

    exploring GET, POST and DELETE API

--- src/app.js
+++ src/app.js
@@ -3,13 +3,34 @@ const express = require("express");
 const app = express();
 
 //response handler
+
+app.get("/user", (req, res)=> {
+    res.send({firstname: "pooja", lastname: "rajput"});
+})
+
+app.post("/user", (req, res)=> {
+    res.send("data successfully send to the DB");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("data deleted successfully");
+})
+
 app.use("/test", (req, res) => {
     res.send("hello");
 })
 
-app.use("/hello", (req, res) => {
-    res.send("hello wolrd");
-})
+// app.use("/hello/2",(req, res) => {
+//     res.send("hello 2");
+// })
+
+// app.use("/hello", (req, res) => {
+//     res.send("hello wolrd");
+// })
+
+// app.use("/", (req, res) => {
+//     res.send("hello hello hello");
+// })
 
 app.listen(7777, () => {
     console.log("server is listening...");
