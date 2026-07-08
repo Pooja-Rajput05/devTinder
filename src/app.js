99594551394b4ba8423a31cb459b7c71ff91b3d7
Pooja-Rajput05 <pooja.kv2005@gmail.com>
2026-07-08 17:21:03 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -4,21 +4,55 @@ const app = express();
 
 //response handler
 
-app.get("/user", (req, res)=> {
-    res.send({firstname: "pooja", lastname: "rajput"});
-})
+app.use(
+    "/user",
+    (req, res, next) => {                          // known as middlewares
+        //route hanlder 1
+        console.log("handling the route user 1");
+        // res.send("Response 1");
+        next();
+    },
+    (req, res, next) => {                             //middlewares
+        //route hanlder 2
+        console.log("handling the route user 2");
+        // res.send("Response 2");
+        next();
+    },
+    (req, res, next) => {                            //middlewares
+        //route hanlder 2
+        console.log("handling the route user 3");
+        // res.send("Response 3");
+        next();
+    },
+    (req, res, next) => {                              //middlewares
+        //route hanlder 2
+        console.log("handling the route user 4");
+        // res.send("Response 4");
+        next();
+    },
+    (req, res, next) => {                             //route handler
+        //route hanlder 2
+        console.log("handling the route user 5");
+        res.send("Response 5");
+        next();
+    },
+);
 
-app.post("/user", (req, res)=> {
-    res.send("data successfully send to the DB");
-})
+// app.get("/user", (req, res)=> {
+//     res.send({firstname: "pooja", lastname: "rajput"});
+// })
 
-app.delete("/user", (req, res) => {
-    res.send("data deleted successfully");
-})
+// app.post("/user", (req, res)=> {
+//     res.send("data successfully send to the DB");
+// })
 
-app.use("/test", (req, res) => {
-    res.send("hello");
-})
+// app.delete("/user", (req, res) => {
+//     res.send("data deleted successfully");
+// })
+
+// app.use("/test", (req, res) => {
+//     res.send("hello");
+// })
 
 // app.use("/hello/2",(req, res) => {
 //     res.send("hello 2");
